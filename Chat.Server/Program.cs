cd91fdd651553e811762f6347e4d47569b5062e2
Humberto Dias <hldtux@gmail.com>
2024-05-05 23:57:53 -0300

    Startup

--- Chat.Server/Program.cs
+++ Chat.Server/Program.cs
@@ -3,20 +3,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel(options =>
+class Program
 {
-    options.ListenAnyIP(5000);
-    // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
-    options.ConfigureEndpointDefaults(endpointOptions =>
+
+    public static void Main(string[] args)
     {
-        endpointOptions.Protocols = HttpProtocols.Http2;
-    });
-});
-builder.Services.AddGrpc();  // MagicOnion depends on ASP.NET Core gRPC service.
-builder.Services.AddMagicOnion();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(5000);
+            // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
+            options.ConfigureEndpointDefaults(endpointOptions =>
+            {
+                endpointOptions.Protocols = HttpProtocols.Http2;
+            });
+        });
+        builder.Services.AddGrpc();  // MagicOnion depends on ASP.NET Core gRPC service.
+        builder.Services.AddMagicOnion();
 
-var app = builder.Build();
-app.MapMagicOnionService();
+        var app = builder.Build();
+        app.MapMagicOnionService();
 
-app.Run();
+        app.Run();
+    }
+
+}
